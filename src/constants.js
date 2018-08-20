e110c8439cac948d16a88fbd94727053255903d7
Bran Liang <lby89757@hotmail.com>
2018-08-20 22:40:03 +0800

    Update the url

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-export const REACT_APP_WS_URI='wss://www.liangboyuan.pub/websocket'
+export const REACT_APP_WS_URI='wss://dev.krae.cn/websocket'
 
-export const REACT_APP_API_URI='https://www.liangboyuan.pub/api'
+export const REACT_APP_API_URI='https://dev.krae.cn/api'
 
-export const REACT_APP_SERVER_URL='https://www.liangboyuan.pub'
+export const REACT_APP_SERVER_URL='https://dev.krae.cn'
 
-export const REACT_APP_CLIENT_URL='https://www.liangboyuan.pub'
+export const REACT_APP_CLIENT_URL='https://dev.krae.cn'
